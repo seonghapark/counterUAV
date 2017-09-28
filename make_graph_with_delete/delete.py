1c002e7f2e5c59e33015d8a8d390bc23ba84280b
seonghye <hansh0713@naver.com>
2017-09-28 16:15:38 -0400

    graph_with_delete_delay

--- make_graph_with_delete/delete.py
+++ make_graph_with_delete/delete.py
@@ -7,28 +7,28 @@ from matplotlib.colors import BoundaryNorm
 #global variables
 dt = 1
 dy = 1
-max_t = 10
-max_y = 10
-set_t = 100
-fig = plt.figure() 
-ax0 = fig.add_subplot(1,1,1)
-bar = False
-
-y = np.mgrid[slice(0, max_y+dy, dy)] #distance numpy array
+max_t = 100
+max_y = 1024
+set_t = 3000
+fig = plt.figure()
 
 data = np.zeros((max_t,max_y)) #define and initialize data to 0
+
+infile = open("dummy.txt",'r') #input data file
 
 #color setting for pcolormesh
 cmap = plt.get_cmap('jet')
 norm = BoundaryNorm([i for i in range(-80,1)], ncolors = cmap.N, clip = True)
+extent = ((-max_t,0,0,max_y))
+im = plt.imshow(data.T,cmap = cmap, norm=norm, animated=True, extent = extent, aspect = 'auto')
+fig.colorbar(im)
 
-infile = open("dummy.txt",'r') #input data file
-
-
+def init():
+	return
 
 def animate(time):
 	global data
-	global bar
+
 	#get data from inputfile and make numpy array
 	line = infile.readline()
 	split_line = line.split()
@@ -39,22 +39,23 @@ def animate(time):
 	data = np.delete(data,0,0)
 	data = np.insert(data, max_t-1, split_line,0)
 
-	#print(data)
+	#draw_graph
+	if(time<max_t):
+		im.set_array(data[max_t-1-time:].T)
+		im.set_extent((0,time+1,0,max_y))
+	else:
+		im.set_array(data.T)
+		im.set_extent((time-max_t+1,time+1,0,max_y))
+	plt.xlim(time-max_t+1,time+1)
 
-	#drawing graph
-	plt.xlim(time-max_t,time) #fix x_scale
-	t=[time,time+1]
-	#print(data[-1:])
-	im = ax0.pcolormesh(t,y, data[-1:].T, cmap = cmap, norm = norm)
-	if(not bar):
-		fig.colorbar(im)
-		bar = True
+	return im,
+
 
 
 plt.xlabel('Time(s)')
 plt.ylabel('Distance(m)')
-plt.ylim(0,10)
+plt.ylim(0,max_y)
 
-ani = animation.FuncAnimation(fig, animate, interval=1000, frames = set_t-1, repeat = False)
+ani = animation.FuncAnimation(fig, animate, init_func = init, interval=1000, frames = set_t-1, repeat = False)
 
 plt.show()
