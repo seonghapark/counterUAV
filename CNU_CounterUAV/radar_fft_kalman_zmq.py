cbaf8e0c392f21d788c7817df4d490ad7ed0912a
htw5295 <xowktktjd@gmail.com>
2018-02-25 21:26:45 +0000

    raspberry pi spi code

--- CNU_CounterUAV/radar_fft_kalman_zmq.py
+++ CNU_CounterUAV/radar_fft_kalman_zmq.py
@@ -4,6 +4,127 @@ import pylab as pl
 from scipy.io import wavfile
 from pykalman import KalmanFilter
 import threading
+import spidev
+import time
+
+#https://github.com/kerrydwong/AD7705X
+#https://github.com/belese/python-AD7705X
+
+#SPI CODE
+REG_CMM = 0x0  # communication register 8 bit
+REG_SETUP = 0x1  # setup register 8 bit
+REG_CLOCK = 0x2  # clock register 8 bit
+REG_DATA = 0x3  # data register 16bit, contains conversion result
+REG_TEST = 0x4  # test register 8 bit, POR 0x0
+REG_NOP = 0x5  # no operation
+REG_OFFSET = 0x6  # offset register 24 bit
+REG_GAIN = 0x7  # gain register 24 bit
+
+# channel selection for AD7705
+#CH1 CH0
+CHN_AIN1 = 0x0
+CHN_AIN2 = 0x1
+
+# output update rate
+# CLK FS1 FS0
+UPDATE_RATE_20 = 0X0  # 20 Hz
+UPDATE_RATE_25 = 0X1  # 25 Hz
+UPDATE_RATE_100 = 0X2  # 100 Hz
+UPDATE_RATE_200 = 0X3  # 200 Hz
+UPDATE_RATE_50 = 0X4  # 50 Hz
+UPDATE_RATE_60 = 0X5  # 60 Hz
+UPDATE_RATE_250 = 0X6  # 250 Hz
+UPDATE_RATE_500 = 0X7  # 500 Hz
+
+#operation mode options
+#MD1 MD0
+MODE_NORMAL = 0x0  # normal mode
+MODE_SELF_CAL = 0x1  # self-calibration
+MODE_ZERO_SCALE_CAL = 0x2  # zero-scale system calibration, POR 0x1F4000, set FSYNC high before calibration, FSYNC low after clibration
+MODE_FULL_SCALE_CAL = 0x3  # full-scale system calibration, POR 0x5761AB, set FSYNC high before calibration, FSYNC low after calibration
+
+# gain setting
+GAIN_1 = 0X0
+GAIN_2 = 0X1
+GAIN_4 = 0X2
+GAIN_8 = 0X3
+GAIN_16 = 0X4
+GAIN_32 = 0X5
+GAIN_64 = 0X6
+GAIN_128 = 0X7
+
+UNIPOLAR = 0x0
+BIPOLAR = 0x1
+
+CLK_DIV_1 = 0x1
+CLK_DIV_2 = 0x2
+
+MODE = 0b11  # SPI_CPHA | SPI_CPOL
+BITS = 8
+SPEED = 50000
+DELAY = 10
+
+class spi_handler():
+    def __init__(self, bus=0, device=0) :
+        self.spi = spidev.SpiDev()
+        self.spi.open(bus, device)
+        self.spi.max_speed_hz = SPEED
+        self.spi.mode = MODE
+        self.spi.bits_per_word = BITS
+        self.reset()
+
+    def initChannel(self, channel, clkDivider=CLK_DIV_1, polarity=BIPOLAR, gain=GAIN_1, updRate=UPDATE_RATE_25):
+        self.setNextOperation(REG_CLOCK, channel, 0)
+        self.writeClockRegister(0, clkDivider, updRate)  # 우리는 레이더에서 싱크를 줘서 안써도 될듯?
+
+        self.setNextPoeration(GET_SETUP, channel, 0)
+        self.writeSetipRegister(MODE_SELF_CAL, gain, polarity, 0, 0)
+
+        while not slef.dataReady(channel):
+            pass
+
+    def setNextOperation(self, reg, channel, readWrite):
+        r = reg << 4 | readWrite << 3 | channel
+        self.spi.xfer([r])
+
+    # CLKDIS : master clock disable bit
+    # CLKDIV : clock divider bit
+    def writeClockRegiseter(self, CLKDIS, CLKDIV, outputUpdateRate):
+        r = CLKDIS << 4 | CLKDIV << 3 | ouputUpdateRate
+        r &= ~(1 << 2)  # clear CLK
+        sekf.spu.xfer([r])
+
+    def writeSetupRegister(self, operationMode, gain, unipolar, buffered, fsync):
+        r = operationMode << 6 | gain << 3 | unipolar << 2 | buffered << 1| fsync
+        self.spi.xfer([r])
+
+    def readADResult(self):
+        b1 = self.spi.xfer([0x0])[0]
+        b2 = self.spi.xfer([0x0])[0]
+        r = int(b1 << 8 | b2)
+
+        return r
+
+    def readADResultRaw(self, channel):
+        while not self.dataReady(channel):
+            pass
+        self.setNextOperation(REG_DATA, channel, 1)
+
+        return self.readADResult()
+
+    def readVoltage(self, channel, vref, factor=1):
+        return float(self.readADResutlRaw(channel)) / 65536.0 * vref * factor
+
+    def dataReady(self, channel):
+        self.setNextOperation(REG_CMM, channel, 1)
+        b1 = self.spi.xfer([0x0])[0]
+
+        return (b1 & 0x80) == 0x0
+
+    def reset(self):
+        for i in range(100):
+            self.spi.xfer([0xff])
+
 
 fs = 44100
 flag = 0
@@ -341,6 +462,13 @@ class kmf_handler:
 
 def main():
     global flag
+
+    spi = spi_handler()
+    spi.initChannel(CHN_AIN1)
+
+    while True:
+        print(spi.readADResultRaw(CHN_AIN1))
+        time.sleep(0.5)
 
     head = Node()
     tail = Node()
