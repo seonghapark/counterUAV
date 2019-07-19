c7f7c090d96b68c979b036caf4bf4157c0b1b11f
Jaehun Jeong <winy713@naver.com>
2019-07-19 10:43:22 +0900

    ros catkin_ws

---
+++ ROS_system/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jhjeong/project/counterUAV/ROS_system/catkin_ws/src
+# Build directory: /home/jhjeong/project/counterUAV/ROS_system/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
